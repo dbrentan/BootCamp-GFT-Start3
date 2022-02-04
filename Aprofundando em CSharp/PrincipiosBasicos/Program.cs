89bcc2a74e02246f43e3b73398db71ca08c8bcba
dbrentan <danielbrentan@gmail.com>
2022-02-04 11:39:01 -0300

    Praticando as aulas do bootcamp da GFT Start3

---
+++ Aprofundando em CSharp/PrincipiosBasicos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrincipiosBasicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-Vindo ao curso de .NET: {i}");
+            }
+
+
+        }
+    }
+}
