11ef3f0f1e73a3e19fa61ef0cdd38329d0476265
dbrentan <danielbrentan@gmail.com>
2022-02-04 14:07:26 -0300

    Demonstração de Value Types

---
+++ Aprofundando em CSharp/DemoValueTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DemoValueTypes
+{
+    public class Program
+    {
+        static int Adicionar20(int x)
+        {
+            return x + 20;
+        }
+        public static void Main()
+        {
+            int a = 2;
+            a = Adicionar20(a);
+
+            Console.WriteLine($"O valor da variável A é {a}");
+
+        }
+    }
+}
